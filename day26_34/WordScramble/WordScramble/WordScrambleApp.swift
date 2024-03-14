cef5c54a4ad351114179a60d3297c2e3dece4cee
jcbura <jasoncbura@gmail.com>
2024-03-14 09:50:10 -0500

    add days 29-30

---
+++ day26_34/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by jason on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
