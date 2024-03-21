163e3fea43fa47031934babee925b8a32bc9db6c
jcbura <jasoncbura@gmail.com>
2024-03-21 12:52:37 -0500

    add days 43-44

---
+++ day36_46/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by jason on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
